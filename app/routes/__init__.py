7b786388d3b8b37ba9b3ca1f5f3ffda3b3821330
Amisha103 <bhatiaamisha74@gmail.com>
2026-02-01 18:04:23 +0530

    business landing page half completed

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,4 +1,8 @@
 from app.routes.landing_routes import landing_bp
+from app.routes.business_routes import business_bp
+
 
 def register_routes(app):
     app.register_blueprint(landing_bp)
+    app.register_blueprint(business_bp, url_prefix="/business")
+
--- app/routes/business_routes.py
+++ app/routes/business_routes.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash
+from flask import Blueprint, render_template
 from app.extensions import db
 from app.models.business import Business
-from app.models.service import Service        
-from app.models.business_image import BusinessImage 
+from app.models.service import Service
+from app.models.business_image import BusinessImage
 
 business_bp = Blueprint(
     "business",
@@ -10,16 +10,35 @@ business_bp = Blueprint(
     template_folder="../templates/business"
 )
 
-@business_bp.route('/<int:business_id>')
-def business_home(business_id):
+@business_bp.route("/<slug>")
+def business_home(slug):
     
-    business = Business.query.get_or_404(business_id)
+    business = Business.query.filter_by(slug=slug).first_or_404()
+
+    
     services = Service.query.filter_by(business_id=business.id).all()
     images = BusinessImage.query.filter_by(business_id=business.id).all()
 
     return render_template(
-        'business_home.html',
+        "business_home.html",
         business=business,
         services=services,
         images=images
     )
+
+@business_bp.route('/<int:business_id>/admin/login', methods=['GET', 'POST'])
+def admin_login(business_id):
+    business = Business.query.get_or_404(business_id)
+    return render_template('admin_login.html', business=business)
+
+@business_bp.route('/<int:business_id>/staff/login', methods=['GET', 'POST'])
+def staff_login(business_id):
+    business = Business.query.get_or_404(business_id)
+    return render_template('staff_login.html', business=business)
+
+@business_bp.route('/<int:business_id>/user/login', methods=['GET', 'POST'])
+def user_login(business_id):
+    business = Business.query.get_or_404(business_id)
+    return render_template('user_login.html', business=business)
+
+
