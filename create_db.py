015a5a2829d3e69064c08d992825b67c78818104
Amisha103 <bhatiaamisha74@gmail.com>
2026-04-05 20:18:15 +0530

    deploying

--- create_db.py
+++ create_db.py
@@ -1,37 +1,15 @@
-import psycopg2
-from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
-from dotenv import load_dotenv
 import os
-
+import psycopg2
+from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_NAME = os.getenv("POSTGRES_DB")
-USER = os.getenv("POSTGRES_USER")
-PASSWORD = os.getenv("POSTGRES_PASSWORD")
-HOST = os.getenv("POSTGRES_HOST")
-PORT = os.getenv("POSTGRES_PORT")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-
-conn = psycopg2.connect(
-    dbname="postgres",
-    user=USER,
-    password=PASSWORD,
-    host=HOST,
-    port=PORT
-)
-
-conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+conn = psycopg2.connect(DATABASE_URL)
 cur = conn.cursor()
 
-cur.execute("SELECT 1 FROM pg_database WHERE datname = %s;", (DB_NAME,))
-exists = cur.fetchone()
-
-if not exists:
-    cur.execute(f"CREATE DATABASE {DB_NAME};")
-    print(f"Database '{DB_NAME}' created successfully")
-else:
-    print(f"Database '{DB_NAME}' already exists")
+print("Connected to database successfully!")
 
 cur.close()
 conn.close()
