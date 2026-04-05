74f596d585cb88c007367807e9ae1208f1d1ae21
Amisha103 <bhatiaamisha74@gmail.com>
2026-04-05 20:30:30 +0530

    db config updated

--- app/config.py
+++ app/config.py
@@ -5,8 +5,11 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key-change-this")
-    SQLALCHEMY_DATABASE_URI = (
-        f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}"
-        f"@{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_PORT')}/{os.getenv('POSTGRES_DB')}"
-    )
+
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+
+    # Fix for Render (postgres → postgresql)
+    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
