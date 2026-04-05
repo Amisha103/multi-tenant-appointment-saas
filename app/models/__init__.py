eed2d8a8cac2f7e40366a3099afcde47517efed4
Amisha103 <bhatiaamisha74@gmail.com>
2026-04-05 21:12:49 +0530

    models init.py updated

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,8 +1,8 @@
 from .business import Business
 from .service import Service
-
 from .business_user import BusinessUser
 from .user import User
 from .staff import Staff
-from app.models.appointment import Appointment
+from .appointment import Appointment
+from .master_service import MasterService
 
--- app/models/base.py
+++
@@ -1,6 +0,0 @@
-from app.extensions import db
-
-class BaseModel(db.Model):
-    __abstract__ = True
-
-    id = db.Column(db.Integer, primary_key=True)
