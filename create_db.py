3191804f3d2988ca950cc591427804def4722f1c
Amisha103 <bhatiaamisha74@gmail.com>
2026-08-02 22:54:07 +0530

    db file temp changed

--- create_db.py
+++ create_db.py
@@ -1,30 +1,8 @@
 import os
-import psycopg2
-import time
-from dotenv import load_dotenv
 
-load_dotenv()
+url = os.getenv("DATABASE_URL")
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+print("DATABASE URL:")
+print(url)
 
-for i in range(10):
-    try:
-        conn = psycopg2.connect(
-            DATABASE_URL,
-            sslmode='require'   # 🔥 IMPORTANT for Render
-        )
-        cur = conn.cursor()
-
-        print("✅ Connected to database successfully!")
-
-        cur.close()
-        conn.close()
-        break
-
-    except psycopg2.OperationalError as e:
-        print(f"❌ Error: {e}")
-        print(f"⏳ Database not ready... retrying ({i+1}/10)")
-        time.sleep(2)
-
-else:
-    print("❌ Could not connect to database after multiple attempts")
+conn = psycopg2.connect(url)
