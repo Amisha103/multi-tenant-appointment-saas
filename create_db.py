22ef253e59b81c12f58a3796eb07fd53c5d5e676
Amisha103 <bhatiaamisha74@gmail.com>
2026-08-02 22:59:18 +0530

    db old file restored

--- create_db.py
+++ create_db.py
@@ -1,8 +1,30 @@
 import os
+import psycopg2
+import time
+from dotenv import load_dotenv
 
-url = os.getenv("DATABASE_URL")
+load_dotenv()
 
-print("DATABASE URL:")
-print(url)
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-conn = psycopg2.connect(url)
+for i in range(10):
+    try:
+        conn = psycopg2.connect(
+            DATABASE_URL,
+            sslmode='require'   # 🔥 IMPORTANT for Render
+        )
+        cur = conn.cursor()
+
+        print("✅ Connected to database successfully!")
+
+        cur.close()
+        conn.close()
+        break
+
+    except psycopg2.OperationalError as e:
+        print(f"❌ Error: {e}")
+        print(f"⏳ Database not ready... retrying ({i+1}/10)")
+        time.sleep(2)
+
+else:
+    print("❌ Could not connect to database after multiple attempts")
